c2193da06c918c06a03690354bb6ea62c1a607b0
Simon <simon.stahlberg@gmail.com>
2023-06-05 22:55:51 +0200

    Add pybind11

--- mimir.cpp
+++ mimir.cpp
@@ -1,23 +1,6 @@
+#include <pybind11/pybind11.h>
 #include <Python.h>
 
-static PyObject* mimir_say_hello(PyObject* self, PyObject* args) {
-    const char* message = "Hello from mimir!";
-    return Py_BuildValue("s", message);
-}
+int add(int a, int b) { return a + b; }
 
-static PyMethodDef mimir_methods[] = {
-    {"SayHello", mimir_say_hello, METH_NOARGS, "Print a hello message."},
-    {NULL, NULL, 0, NULL}
-};
-
-static struct PyModuleDef mimir_module = {
-    PyModuleDef_HEAD_INIT,
-    "mimir",
-    "A sample C++ module for Python",
-    -1,
-    mimir_methods
-};
-
-PyMODINIT_FUNC PyInit_mimir(void) {
-    return PyModule_Create(&mimir_module);
-}
+PYBIND11_MODULE(mimir, m) { m.def("add", &add, "A function that adds two numbers"); }
