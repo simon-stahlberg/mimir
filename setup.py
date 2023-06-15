577cd53ea7b196d6a0c20efe4feeab4404533ca1
Simon <simon.stahlberg@gmail.com>
2023-06-15 14:09:55 +0200

    Add PyPi support

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from skbuild import setup
+
+# Set the directory containing your CMakeLists.txt
+cmake_dir = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='mimir',
+    version='0.1.0',
+    description='Mimir: Lifted PDDL parsing and expansion library.',
+    author='Simon',
+    author_email='your@email.com',
+    cmake_args=[
+        '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' +
+            os.path.join(cmake_dir, 'mimir')
+    ],
+    packages=['mimir'],
+    cmake_source_dir='.'
+)
