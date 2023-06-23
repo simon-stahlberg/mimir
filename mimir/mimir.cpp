d68d7dedaae2cd0542d52d49f2e2d2cae6ff73c2
Simon <simon.stahlberg@gmail.com>
2023-06-23 12:50:07 +0200

    Add argument names for 'pack_objects_ids...'

--- mimir/mimir.cpp
+++ mimir/mimir.cpp
@@ -135,7 +135,7 @@ PYBIND11_MODULE(mimir, m)
         .def("get_dynamic_atoms", &formalism::StateImpl::get_dynamic_atoms)
         // .def("get_problem", &formalism::StateImpl::get_problem)  // Forward declare Problem.
         .def("get_atoms_by_predicate", &formalism::StateImpl::get_atoms_grouped_by_predicate)
-        .def("pack_object_ids_by_predicate_id", &formalism::StateImpl::pack_object_ids_by_predicate_id, "include_types", "include_goal")
+        .def("pack_object_ids_by_predicate_id", &formalism::StateImpl::pack_object_ids_by_predicate_id, "include_types"_a, "include_goal"_a)
         .def("__repr__", [](const formalism::StateImpl& state) { return "<State '" + std::to_string(state.hash()) + "'>"; });
 
     py::class_<formalism::ActionImpl, formalism::Action>(m, "Action")
