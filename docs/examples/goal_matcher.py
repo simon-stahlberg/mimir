95e3eb36fc7d753606803f7229cc22f185021024
Simon <simon.stahlberg@gmail.com>
2023-11-13 20:49:07 +0100

    Add timings to example

--- docs/examples/goal_matcher.py
+++ docs/examples/goal_matcher.py
@@ -1,6 +1,7 @@
-import argparse
-import mimir
+import time
 import random
+import mimir
+import argparse
 
 from pathlib import Path
 
@@ -14,8 +15,11 @@ def main():
     args = parse_arguments()
     domain = mimir.DomainParser(str(args.domain_path)).parse()
     problem = mimir.ProblemParser(str(args.problem_path)).parse(domain)
+    time_start = time.time()
     successor_generator = mimir.GroundedSuccessorGenerator(problem)
     state_space = mimir.StateSpace.new(problem, successor_generator)
+    time_end = time.time()
+    print(f'StateSpace: {state_space.num_states()} [{time_end - time_start:.1f} seconds]')
     if state_space is None:
         print('Too many states. Aborting.')
         exit(1)
@@ -26,9 +30,12 @@ def main():
         else: return domain.predicates[random.randint(0, len(domain.predicates) - 1)].as_atom()
     goal = [get_random_atom(), get_random_atom()]  # Construct your quantified goal here
     print(f'Quantified goal: {goal}')
+    time_start = time.time()
     state, distance = goal_matcher.best_match(goal)
+    time_end = time.time()
     print(f'Distance to closest state matching the goal: {distance}')
     print(f'State: {"-" if state is None else state.get_atoms()}')
+    print(f'Time: {time_end - time_start:.5f} seconds')
 
 if __name__ == '__main__':
     main()
