374567b1e70d5cd893feb5041a1248d27faf3bfa
Dominik Drexler <dominik.drexler@gmx.net>
2024-01-12 14:05:25 +0100

    work on integration tests

---
+++ test/integration/dependencies/README.md
@@ -0,0 +1,3 @@
+# Conventions for Adding Dependencies
+
+- We use the naming convention <project_name>_<dependency_name> to allow using different versions when used in other projects to avoid target name clashes.
---
+++ test/integration/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <loki/domain/parser.hpp>
+#include <loki/problem/parser.hpp>
+
+#include <iostream>
+
+
+int main() {
+    // Parse the domain
+    auto domain_parser = loki::DomainParser("data/gripper/domain.pddl");
+    const auto domain = domain_parser.get_domain();
+    std::cout << *domain << std::endl;
+
+    // Parse the problem
+    const auto problem_parser = loki::ProblemParser("data/gripper/p-2-0.pddl", domain_parser);
+    const auto problem = problem_parser.get_problem();
+    std::cout << *problem << std::endl;
+
+    return 0;
+}
