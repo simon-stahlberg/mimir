6f2fb3e070630d19782793dedfc5cf6cc1c57c10
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-01 22:49:37 +0100

    .

--- tests/unit/algorithms/memory_pool.cpp
+++ tests/unit/algorithms/memory_pool.cpp
@@ -9,25 +9,30 @@ namespace mimir::tests
 {
 
 TEST(MimirTests, AlgorithmsMemoryPoolTest) {
-    MemoryPool<std::vector<bool>> pool;
+    MemoryPool<std::vector<int>> pool;
     EXPECT_EQ(pool.get_size(), 0);
     auto object_0 = pool.get_or_allocate();
     EXPECT_EQ(pool.get_size(), 1);
     EXPECT_EQ(pool.get_num_free(), 0);
     {
         auto object_1 = pool.get_or_allocate();
+        object_1->push_back(42);
         EXPECT_EQ(pool.get_size(), 2);
         EXPECT_EQ(pool.get_num_free(), 0);
+        EXPECT_EQ(object_1->size(), 1);
+        EXPECT_EQ(object_1->back(), 42);
         // destructor of object_1 is called
     }
     // pool size remains 2
     EXPECT_EQ(pool.get_size(), 2);
     EXPECT_EQ(pool.get_num_free(), 1);
 
-    // reuse element
+    // reuse element, it still contains the data.
     auto object_1 = pool.get_or_allocate();
     EXPECT_EQ(pool.get_size(), 2);
     EXPECT_EQ(pool.get_num_free(), 0);
+    EXPECT_EQ(object_1->size(), 1);
+    EXPECT_EQ(object_1->back(), 42);
 }
 
 
