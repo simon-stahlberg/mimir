f155b06a8661b9dcd3526d51cd33efa1f1d8c3aa
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-02 23:24:31 +0100

    added data

--- tests/unit/search/planners/single.cpp
+++ tests/unit/search/planners/single.cpp
@@ -9,8 +9,8 @@ namespace mimir::tests
 {
 
 TEST(MimirTests, SearchPlannersSingleTest) {
-    const auto domain_file = std::string("domain.pddl");
-    const auto problem_file = std::string("problem.pddl");
+    const auto domain_file = fs::path(std::string(DATA_DIR) + "gripper/domain.pddl");
+    const auto problem_file = fs::path(std::string(DATA_DIR) + "gripper/problem.pddl");
     auto planner = Planner<SingleTag<AStarTag<GroundedTag, BlindTag>>>(domain_file, problem_file);
     const auto [status, plan] = planner.find_solution();
 }
