ce199371275b1fa5c9c1fff1483b94d3f61b5b98
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-03 23:18:48 +0100

    added parsing of atom and literal

---
+++ src/formalism/domain/parsers/atom.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler and Simon Stahlberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <mimir/formalism/domain/parsers/atom.hpp>
+
+#include <mimir/formalism/domain/parsers/predicate.hpp>
+#include <mimir/formalism/domain/parsers/term.hpp>
+
+
+namespace mimir 
+{
+    Atom parse(loki::pddl::Atom atom, PDDLFactories& factories) {
+        return factories.atoms.get_or_create<AtomImpl>(parse(atom->get_predicate(), factories), parse(atom->get_terms(), factories));
+    }
+
+    AtomList parse(loki::pddl::AtomList atom_list, PDDLFactories& factories) {
+        auto result_atom_list = AtomList();
+        for (const auto& atom : atom_list) {
+            result_atom_list.push_back(parse(atom, factories));
+        }
+        return result_atom_list;
+    }
+}
---
+++ src/formalism/domain/parsers/literal.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler and Simon Stahlberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <mimir/formalism/domain/parsers/literal.hpp>
+
+#include <mimir/formalism/domain/parsers/atom.hpp>
+
+
+namespace mimir 
+{
+    Literal parse(loki::pddl::Literal literal, PDDLFactories& factories) {
+        return factories.literals.get_or_create<LiteralImpl>(literal->is_negated(), parse(literal->get_atom(), factories));
+    }
+
+    LiteralList parse(loki::pddl::LiteralList literal_list, PDDLFactories& factories) {
+        auto result_literal_list = LiteralList();
+        for (const auto& literal : literal_list) {
+            result_literal_list.push_back(parse(literal, factories));
+        }
+        return result_literal_list;
+    }
+}
