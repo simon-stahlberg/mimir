06f5ccf4dcd52953eb455ff961bd21e1328fa039
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-18 00:03:09 +0100

    fix ub in loki

--- src/formalism/parser.cpp
+++ src/formalism/parser.cpp
@@ -10,8 +10,7 @@
 namespace mimir 
 {
     PDDLParser::PDDLParser(const fs::path& domain_file_path, const fs::path& problem_file_path)
-    { 
-        
+    {   
         /* Parse the domain */
         std::cout << "Loki parsing domain:" << std::endl;
         auto domain_parser = loki::DomainParser(domain_file_path);
