e9616d4113db6e24b16d7b6e9d153e5575fb7729
Dominik Drexler <dominik.drexler@gmx.net>
2024-03-15 12:23:03 +0100

    changed folder structure

--- python/src/develop_bindings.py
+++ python/src/develop_bindings.py
@@ -4,15 +4,14 @@ import sys
 
 from pathlib import Path
 
-ROOT_DIR = (Path.cwd().parent).absolute()
+ROOT_DIR = (Path.cwd().parent.parent).absolute()
 LIB_PATH = ROOT_DIR / "build" / "python"
 
 sys.path.append(str(LIB_PATH))
 
-from src.mimir.formalism import PDDLParser
-from src.mimir.search import *
+from mimir.formalism import PDDLParser
+from mimir.search import *
 
 
 if __name__ == "__main__":
-
     parser = PDDLParser(str(ROOT_DIR / "data" / "gripper" / "domain.pddl"), str(ROOT_DIR / "data" / "gripper" / "problem.pddl"))
