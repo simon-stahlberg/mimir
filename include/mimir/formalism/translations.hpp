e1420d208c94d2077e262355e7ae76c0bb94af47
Dominik Drexler <dominik.drexler@gmx.net>
2024-03-19 18:46:57 +0100

    added some files

---
+++ include/mimir/formalism/translations.hpp
@@ -0,0 +1,30 @@
+#ifndef MIMIR_FORMALISM_TRANSLATIONS_HPP_
+#define MIMIR_FORMALISM_TRANSLATIONS_HPP_
+
+#include "mimir/formalism/declarations.hpp"
+
+#include <tuple>
+#include <variant>
+#include <vector>
+
+namespace mimir
+{
+
+/// @brief Compute the positive normalized problem into a new PDDLFactory.
+/// @param problem
+/// @return
+extern std::tuple<Problem, PDDLFactories> to_positive_normalized_problem(const Problem& problem);
+
+/// @brief Compute the effect normalized problem into a new PDDLFactory.
+/// @param problem
+/// @return
+extern std::tuple<Problem, PDDLFactories> to_effect_normalized_problem(const Problem& problem);
+
+/// @brief Compute the delete relaxed problem where all negative effects are removed.
+/// @param problem
+/// @return
+extern std::tuple<Problem, PDDLFactories> to_delete_relaxed_problem(const Problem& problem);
+
+}
+
+#endif
---
+++ include/mimir/search/relaxed_planning_graph/rpg.hpp
@@ -0,0 +1,12 @@
+#ifndef MIMIR_SEARCH_RELAXED_PLANNING_GRAPH_RPG_HPP_
+#define MIMIR_SEARCH_RELAXED_PLANNING_GRAPH_RPG_HPP_
+
+#include <variant>
+#include <vector>
+
+namespace mimir::rpg
+{
+
+}
+
+#endif
