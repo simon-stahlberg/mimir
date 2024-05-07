576b27627256170b363274c03312a5a3ceed3131
Dominik Drexler <dominik.drexler@gmx.net>
2024-05-07 10:22:51 +0200

    work on splitting assignment set

--- include/mimir/search/applicable_action_generators/dense_lifted/assignment_set.hpp
+++ include/mimir/search/applicable_action_generators/dense_lifted/assignment_set.hpp
@@ -3,6 +3,7 @@
 
 #include "mimir/formalism/declarations.hpp"
 
+#include <cstdint>
 #include <vector>
 
 namespace mimir
@@ -12,6 +13,65 @@ namespace consistency_graph
 class Vertex;
 class Edge;
 }
+
+// TODO: this will make the function m_f easier to understand.
+// It will also make evaluations more efficient since they are closer together in memory.
+class UnaryAssignmentSet
+{
+private:
+    Problem m_problem;
+    std::vector<std::vector<bool>> m_f;
+
+    struct UnaryAssignment
+    {
+        uint32_t position;
+        uint32_t object;
+    };
+
+    static size_t get_assignment_position(const UnaryAssignment& assignment, uint32_t arity, uint32_t num_objects);
+
+    static size_t get_num_assignments(uint32_t arity, uint32_t num_objects);
+
+public:
+    /// @brief Construct from a given set of ground atoms.
+    UnaryAssignmentSet(Problem problem, const GroundAtomList& atoms);
+
+    /// @brief Return true iff all literals are consistent with
+    /// 1. the assignment set, and 2. the vertex of the consistency graph.
+    ///
+    /// The meaning is that the assignment [x/o] is a legal assignment,
+    /// and therefore the vertex must be part of the consistency graph.
+    bool literal_all_consistent(const std::vector<Literal>& literals, const consistency_graph::Vertex& consistent_vertex) const;
+};
+
+class BinaryAssignmentSet
+{
+private:
+    Problem m_problem;
+    std::vector<std::vector<bool>> m_f;
+
+    struct BinaryAssignment
+    {
+        uint32_t first_position;
+        uint32_t first_object;
+        uint32_t second_position;
+        uint32_t second_object;
+    };
+
+    static size_t get_assignment_position(const BinaryAssignment& assignment, uint32_t arity, uint32_t num_objects);
+
+    static size_t get_num_assignments(uint32_t arity, uint32_t num_objects);
+
+public:
+    /// @brief Construct from a given set of ground atoms.
+    BinaryAssignmentSet(Problem problem, const GroundAtomList& atoms);
+
+    /// @brief Return true iff all literals are consistent with
+    /// 1. the assignment set, and 2. the edge of the consistency graph.
+    ///
+    /// The meaning of the result being true is that the edge remains consistent.
+    bool literal_all_consistent(const std::vector<Literal>& literals, const consistency_graph::Edge& consistent_edge) const;
+};
 
 /// @brief AssignmentSet is a helper class representing a set of functions
 /// f : Predicates x Params(A) x Object x Params(A) x Object -> {true, false} where
