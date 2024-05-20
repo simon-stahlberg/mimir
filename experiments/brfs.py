79cd5372354b0bebab57462422ed79bdedb25027
Dominik Drexler <dominik.drexler@gmx.net>
2024-05-20 23:39:14 +0200

    set to default experiment settings

--- experiments/brfs.py
+++ experiments/brfs.py
@@ -33,8 +33,6 @@ NODE = platform.node()
 REMOTE = re.match(r"tetralith\d+.nsc.liu.se|n\d+", NODE)
 if REMOTE:
     ENV = TetralithEnvironment(
-        memory_per_cpu="2900M",  # 2900M * 32 = 92.8G
-        cpus_per_task=1,
         setup=TetralithEnvironment.DEFAULT_SETUP,
         extra_options="#SBATCH --account=naiss2023-5-314")
     SUITE = [
