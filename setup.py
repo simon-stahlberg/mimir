c53b923fa3a2f5bbe0f3f359dfcb9b6ef3213711
Dillon <dillon.chen1@gmail.com>
2024-05-28 18:15:10 +0200

    0.1.3 for testpypi

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pymimir-dzc-fork",
-    version="0.1.2",
+    version="0.1.3",
     author="Dillon Z. Chen",
     author_email="dillon.chen1@gmail.com",
     description="Fork of Simon's Mimir: PDDL Parser and Planner Toolkit",
@@ -87,5 +87,5 @@ setup(
     ext_modules=[CMakeExtension("mimir")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
