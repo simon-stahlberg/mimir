6b73b7ac451bfb9674e8fafcddfc37568f91bd16
Dominik Drexler <dominik.drexler@gmx.net>
2024-06-18 21:18:35 +0200

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
-__version__ = "0.9.22"
+__version__ = "0.9.23"
 HERE = Path(__file__).resolve().parent
 
 
