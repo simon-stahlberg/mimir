7e682e214136960fdf168da63394ab06228af65b
Dominik Drexler <dominik.drexler@gmx.net>
2024-06-28 23:57:36 +0200

    added test to parse a dl feature using the grammar specification parser

--- tests/unit/languages/description_logics/parser.cpp
+++ tests/unit/languages/description_logics/parser.cpp
@@ -25,4 +25,15 @@ TEST(MimirTests, LanguagesDescriptionLogicsParserTest)
     EXPECT_NO_THROW(dl::parse_ast(text, dl::grammar(), ast));
 }
 
+TEST(MimirTests, LanguagesDescriptionLogicsParser2Test)
+{
+    auto ast = dl::ast::Grammar();
+
+    auto text = std::string(R"(
+<concept_x> ::= @concept_and @concept_and @concept_predicate_state "predicate1" @concept_predicate_goal "predicate1" @concept_predicate_state "predicate2"
+)");
+
+    EXPECT_NO_THROW(dl::parse_ast(text, dl::grammar(), ast));
+}
+
 }
