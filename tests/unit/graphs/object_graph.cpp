c4f2f91c823b00835347477c310a151f853ea9e2
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2024-07-09 19:35:10 +0200

    Adapt object graph test to new API

--- tests/unit/graphs/object_graph.cpp
+++ tests/unit/graphs/object_graph.cpp
@@ -90,7 +90,7 @@ TEST(MimirTests, GraphsObjectGraphPruningTest)
         bool prune(const GroundLiteral<Fluent>) const override { return true; }
         bool prune(const GroundLiteral<Derived>) const override { return true; }
     };
-    auto object_graph_factory = ObjectGraphFactory(state_space.get_pddl_parser()->get_problem(), state_space.get_pddl_parser()->get_factories());
+    auto object_graph_factory = ObjectGraphFactory(state_space.get_problem(), state_space.get_pddl_factories());
     auto nauty_graph_factory = nauty_wrapper::SparseGraphFactory();
     auto certificates = std::unordered_set<Certificate, loki::Hash<Certificate>, loki::EqualTo<Certificate>> {};
     for (const auto& state : state_space.get_states())
