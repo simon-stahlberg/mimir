57afc53668b46c5a8e3a5a14632b07ca93c0642c
Dominik Drexler <dominik.drexler@gmx.net>
2024-08-09 23:11:56 +0200

    added goal count heuristic

--- python/tests/search/test_astar_search.py
+++ python/tests/search/test_astar_search.py
@@ -14,6 +14,26 @@ class CustomBlindHeuristic(IHeuristic):
         return 0.
 
 
+class CustomGoalCountHeuristic(IHeuristic):
+    def __init__(self, problem : Problem, pddl_factories: PDDLFactories):
+        IHeuristic.__init__(self)  # Without this, a TypeError is raised.
+        self.problem = problem
+        self.pddl_factories = pddl_factories
+
+        self.num_goal_literals = len(self.problem.get_fluent_goal_condition()) + len(self.problem.get_derived_goal_condition())
+
+    def compute_heuristic(self, state : State) -> float:
+        num_satisfied_goal_literals = 0
+        for literal in self.problem.get_fluent_goal_condition():
+            if state.literal_holds(literal):
+                num_satisfied_goal_literals += 1
+        for literal in self.problem.get_derived_goal_condition():
+            if state.literal_holds(literal):
+                num_satisfied_goal_literals += 1
+
+        return self.num_goal_literals - num_satisfied_goal_literals
+
+
 class CustomAStarAlgorithmEventHandler(AStarAlgorithmEventHandlerBase):
     def __init__(self, quiet = True):
         """
@@ -67,9 +87,12 @@ def test_astar_search():
     parser = PDDLParser(domain_filepath, problem_filepath)
     lifted_applicable_action_generator = LiftedApplicableActionGenerator(parser.get_problem(), parser.get_pddl_factories())
     state_repository = StateRepository(lifted_applicable_action_generator)
+
     blind_heuristic = CustomBlindHeuristic()
+    goal_count_heuristic = CustomGoalCountHeuristic(parser.get_problem(), parser.get_pddl_factories())
+
     event_handler = CustomAStarAlgorithmEventHandler(False)
-    astar_search_algorithm = AStarAlgorithm(lifted_applicable_action_generator, state_repository, blind_heuristic, event_handler)
+    astar_search_algorithm = AStarAlgorithm(lifted_applicable_action_generator, state_repository, goal_count_heuristic, event_handler)
     search_status, plan = astar_search_algorithm.find_solution()
 
     assert search_status == SearchStatus.SOLVED
