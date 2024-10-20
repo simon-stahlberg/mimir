ad7e2b7378d84fef82c8732a72f1cbba5eb7a286
Dominik Drexler <dominik.drexler@gmx.net>
2024-10-20 11:31:07 +0200

    k-wl interface now clearly separates initialization and refinement. Some data structures have been changed

--- include/mimir/graphs/algorithms/kwl.hpp
+++ include/mimir/graphs/algorithms/kwl.hpp
@@ -23,45 +23,46 @@
 namespace mimir
 {
 
-// https://arxiv.org/pdf/1907.09582
-
-// TODO: we need an effective way to compute neighbors of a k-tuple!
-
+/// @brief `WeisfeilerLeman` implements the k-dimensional Weisfeiler-Leman algorithm.
+/// Source: https://arxiv.org/pdf/1907.09582
+/// @tparam k is the dimensionality.
 template<size_t k>
 class WeisfeilerLeman
 {
 public:
-    /// @brief Refine the colors of the unstable k-tuples.
-    /// @tparam G
-    /// @param graph
-    /// @param in_out_unstable_vertex_k_tuples
-    /// @param in_out_k_tuple_colors
-    /// @return
+    // The algorithm ensures lexicographically sorted k-tuples
+    using KTuple = std::array<Index, k>;
+    using KTupleToColor = std::unordered_map<KTuple, Color>;
+
+    /// @brief Initialization step (lines 1-2)
+    /// @tparam G is the type of the given graph.
+    /// @param graph is the given graph.
+    /// @param vertex_colors are the given vertex colors.
+    /// @param out_k_coloring is the initial coloring of all ordered k-tuples.
+    /// @param work_list is the set of unstable colors that require further refinement.
     template<typename G>
     requires IsVertexListGraph<G> && IsIncidenceGraph<G>
-    bool refine_coloring(const G& graph, std::vector<std::pair<Index, std::array<Index, k>>>& in_out_unstable_vertex_k_tuples, ColorMap& in_out_k_tuple_colors);
+    void initialize(const G& graph, const ColorMap& vertex_colors, KTupleToColor& out_k_coloring, ColorSet& work_list);
 
-    /// @brief Compress the colors of each k-tuple of vertices into their (ordered) isomorphism type.
-    /// @param vertex_k_tuples
-    /// @param vertex_colors
-    /// @return
-    ColorList compute_initial_k_coloring(const std::vector<std::pair<Index, std::array<Index, k>>>& vertex_k_tuples, const ColorMap& vertex_colors);
-
-    /// @brief Compute all ordered k-tuples of vertices of a given graph.
-    /// @tparam G
-    /// @param graph
-    /// @return
+    /// @brief Refinement step (lines 3-18)
+    /// @tparam G is the type of the given graph.
+    /// @param graph is the given graph.
+    /// @param out_k_coloring is the current coloring of all ordered k-tuples.
+    /// @param work_list is the set of unstable colors that require further refinement.
     template<typename G>
     requires IsVertexListGraph<G> && IsIncidenceGraph<G>
-    static std::vector<std::pair<Index, std::array<Index, k>>> compute_all_ordered_vertex_k_tuples(const G& graph);
+    void refine_coloring(const G& graph, KTupleToColor& out_k_coloring, ColorSet& work_list);
 
 private:
-    // TODO: add mapping for ordered isomorphism types
+    /* Compression used in line 16 to map (c_1^i, ...,c_k^i) = (c_1^j, ...,c_k^j) to the same integer color. */
+    using KColorCompressionFunction = std::unordered_map<std::array<Color, k>, Color>;
 
-    using Configuration = std::pair<std::array<Color, k>, std::vector<std::tuple<Index, Color, Count>>>;
-    using CompressionFunction = std::unordered_map<Configuration, Color>;
+    KColorCompressionFunction m_f;
 
-    CompressionFunction m_f;
+    /* Compression used in line 18 to map (C(v), (c_1^1, ...,c_k^1), ..., (c_1^r, ...,c_k^r)) to an integer color */
+    using ConfigurationCompressionFunction = std::unordered_map<std::pair<Color, ColorList>, Color>;
+
+    ConfigurationCompressionFunction m_g;
 };
 
 }
