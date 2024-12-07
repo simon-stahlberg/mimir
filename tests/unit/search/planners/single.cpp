29e91cacf34c61b18a4af58f8c60ddb8d73fe916
Dominik Drexler <dominik.drexler@gmx.net>
2024-12-07 17:17:37 +0100

    .

--- tests/unit/search/planners/single.cpp
+++ tests/unit/search/planners/single.cpp
@@ -18,6 +18,7 @@
 #include "mimir/formalism/parser.hpp"
 #include "mimir/search/algorithms.hpp"
 #include "mimir/search/applicable_action_generators.hpp"
+#include "mimir/search/axiom_evaluators.hpp"
 #include "mimir/search/heuristics.hpp"
 #include "mimir/search/planners.hpp"
 
@@ -32,9 +33,10 @@ TEST(MimirTests, SearchPlannersSingleTest)
     const auto domain_file = fs::path(std::string(DATA_DIR) + "gripper/domain.pddl");
     const auto problem_file = fs::path(std::string(DATA_DIR) + "gripper/test_problem.pddl");
     auto parser = PDDLParser(domain_file, problem_file);
-    auto successor_generator = std::make_shared<LiftedApplicableActionGenerator>(parser.get_problem(), parser.get_pddl_repositories());
+    auto applicable_action_generator = std::make_shared<LiftedApplicableActionGenerator>(parser.get_problem(), parser.get_pddl_repositories());
+    auto axiom_evaluator = std::make_shared<LiftedAxiomEvaluator>(parser.get_problem(), parser.get_pddl_repositories());
     auto blind_heuristic = std::make_shared<BlindHeuristic>();
-    auto lifted_astar = std::make_shared<AStarAlgorithm>(successor_generator, blind_heuristic);
+    auto lifted_astar = std::make_shared<AStarAlgorithm>(applicable_action_generator, axiom_evaluator, blind_heuristic);
     auto planner = SinglePlanner(lifted_astar);
     const auto [status, plan] = planner.find_solution();
 }
