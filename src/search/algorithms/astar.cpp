463815300d4eb641ea640308bb156b3dad0650d5
Dominik Drexler <drexlerd93@gmail.com>
2024-12-21 01:22:08 +0100

    update experiments description slightly

--- src/search/algorithms/astar.cpp
+++ src/search/algorithms/astar.cpp
@@ -160,10 +160,10 @@ SearchResult find_solution_astar(std::shared_ptr<IApplicableActionGenerator> app
 
         if (search_node_f_value > f_value)
         {
+            f_value = search_node_f_value;
             applicable_action_generator->on_finish_search_layer();
             state_repository->get_axiom_evaluator()->on_finish_search_layer();
             event_handler->on_finish_f_layer(f_value);
-            f_value = search_node_f_value;
         }
 
         /* Test whether state achieves the dynamic goal. */
