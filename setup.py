7101c228e9a2dbdfea363715a36d600f55766449
Simon <simon.stahlberg@gmail.com>
2024-12-27 19:21:58 +0100

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
-__version__ = "0.2.0"
+__version__ = "0.10.0"
 HERE = Path(__file__).resolve().parent
 
 
