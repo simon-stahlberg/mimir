9e2cdd6a823b9184dc89ee0fd52628a13bb75c99
Dominik Drexler <drexlerd93@gmail.com>
2025-03-16 14:52:48 +0100

    some work on adding dl to python

--- python/src/pymimir/advanced/languages/description_logics/bindings.cpp
+++ python/src/pymimir/advanced/languages/description_logics/bindings.cpp
@@ -14,12 +14,277 @@ namespace mimir::bindings
 class IPyDLVisitor : public dl::Visitor
 {
 public:
-    NB_TRAMPOLINE(dl::Visitor, 1);
+    NB_TRAMPOLINE(dl::Visitor, 40);
 
     /* Trampoline (need one for each virtual function) */
     void visit(dl::ConceptBot constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptTop constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptAtomicState<formalism::Static> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptAtomicState<formalism::Fluent> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptAtomicState<formalism::Derived> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptAtomicGoal<formalism::Static> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptAtomicGoal<formalism::Fluent> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptAtomicGoal<formalism::Derived> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptIntersection constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptUnion constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptNegation constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptValueRestriction constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptExistentialQuantification constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptRoleValueMapContainment constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptRoleValueMapEquality constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::ConceptNominal constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+
+    void visit(dl::RoleUniversal constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleAtomicState<formalism::Static> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleAtomicState<formalism::Fluent> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleAtomicState<formalism::Derived> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleAtomicGoal<formalism::Static> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleAtomicGoal<formalism::Fluent> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleAtomicGoal<formalism::Derived> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleIntersection constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleUnion constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleComplement constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleInverse constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleComposition constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleTransitiveClosure constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleReflexiveTransitiveClosure constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleRestriction constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::RoleIdentity constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+
+    void visit(dl::BooleanAtomicState<formalism::Static> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::BooleanAtomicState<formalism::Fluent> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::BooleanAtomicState<formalism::Derived> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::BooleanNonempty<dl::Concept> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::BooleanNonempty<dl::Role> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+
+    void visit(dl::NumericalCount<dl::Concept> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::NumericalCount<dl::Role> constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
+    void visit(dl::NumericalDistance constructor) override { NB_OVERRIDE_PURE(visit, constructor); }
 };
 
-void bind_languages_description_logics(nb::module_& m) {}
+void bind_languages_description_logics(nb::module_& m)
+{
+    nb::enum_<dl::cnf_grammar::GrammarSpecificationEnum>(m, "GrammarSpecificationEnum")  //
+        .value("FRANCES_ET_AL_AAAI2021", dl::cnf_grammar::GrammarSpecificationEnum::FRANCES_ET_AL_AAAI2021);
+
+    nb::class_<dl::ConstructorRepositories>(m, "ConstructorRepositories")
+        .def(nb::init<>())
+        .def("get_or_create_concept_bot", &dl::ConstructorRepositories::get_or_create_concept_bot, nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_top", &dl::ConstructorRepositories::get_or_create_concept_top, nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_intersection",
+             &dl::ConstructorRepositories::get_or_create_concept_intersection,
+             "left_concept"_a,
+             "right_concept"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_union",
+             &dl::ConstructorRepositories::get_or_create_concept_union,
+             "left_concept"_a,
+             "right_concept"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_negation", &dl::ConstructorRepositories::get_or_create_concept_negation, "concept_"_a, nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_value_restriction",
+             &dl::ConstructorRepositories::get_or_create_concept_value_restriction,
+             "role"_a,
+             "concept_"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_existential_quantification",
+             &dl::ConstructorRepositories::get_or_create_concept_existential_quantification,
+             "role"_a,
+             "concept_"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_role_value_map_containment",
+             &dl::ConstructorRepositories::get_or_create_concept_role_value_map_containment,
+             "left_role"_a,
+             "right_role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_role_value_map_equality",
+             &dl::ConstructorRepositories::get_or_create_concept_role_value_map_equality,
+             "left_role"_a,
+             "right_role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_nominal", &dl::ConstructorRepositories::get_or_create_concept_nominal, "object"_a, nb::rv_policy::reference_internal)
+
+        /* Roles */
+        .def("get_or_create_role_universal", &dl::ConstructorRepositories::get_or_create_role_universal, nb::rv_policy::reference_internal)
+        .def("get_or_create_role_intersection",
+             &dl::ConstructorRepositories::get_or_create_role_intersection,
+             "left_role"_a,
+             "right_role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_union",
+             &dl::ConstructorRepositories::get_or_create_role_union,
+             "left_role"_a,
+             "right_role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_complement", &dl::ConstructorRepositories::get_or_create_role_complement, "role"_a, nb::rv_policy::reference_internal)
+        .def("get_or_create_role_inverse", &dl::ConstructorRepositories::get_or_create_role_inverse, "role"_a, nb::rv_policy::reference_internal)
+        .def("get_or_create_role_composition",
+             &dl::ConstructorRepositories::get_or_create_role_composition,
+             "left_role"_a,
+             "right_role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_transitive_closure",
+             &dl::ConstructorRepositories::get_or_create_role_transitive_closure,
+             "role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_reflexive_transitive_closure",
+             &dl::ConstructorRepositories::get_or_create_role_reflexive_transitive_closure,
+             "role"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_restriction",
+             &dl::ConstructorRepositories::get_or_create_role_restriction,
+             "role"_a,
+             "concept_"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_identity", &dl::ConstructorRepositories::get_or_create_role_identity, "concept_"_a, nb::rv_policy::reference_internal)
+
+        .def("get_or_create_concept_atomic_state_static",
+             &dl::ConstructorRepositories::get_or_create_concept_atomic_state<Static>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_atomic_state_fluent",
+             &dl::ConstructorRepositories::get_or_create_concept_atomic_state<Fluent>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_atomic_state_derived",
+             &dl::ConstructorRepositories::get_or_create_concept_atomic_state<Derived>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+
+        .def("get_or_create_concept_atomic_goal_static",
+             &dl::ConstructorRepositories::get_or_create_concept_atomic_goal<Static>,
+             "predicate"_a,
+             "is_negated"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_atomic_goal_fluent",
+             &dl::ConstructorRepositories::get_or_create_concept_atomic_goal<Fluent>,
+             "predicate"_a,
+             "is_negated"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_concept_atomic_goal_derived",
+             &dl::ConstructorRepositories::get_or_create_concept_atomic_goal<Derived>,
+             "predicate"_a,
+             "is_negated"_a)
+
+        .def("get_or_create_role_atomic_state_static",
+             &dl::ConstructorRepositories::get_or_create_role_atomic_state<Static>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_atomic_state_fluent",
+             &dl::ConstructorRepositories::get_or_create_role_atomic_state<Fluent>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_atomic_state_derived",
+             &dl::ConstructorRepositories::get_or_create_role_atomic_state<Derived>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+
+        .def("get_or_create_role_atomic_goal_static",
+             &dl::ConstructorRepositories::get_or_create_role_atomic_goal<Static>,
+             "predicate"_a,
+             "is_negated"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_atomic_goal_fluent",
+             &dl::ConstructorRepositories::get_or_create_role_atomic_goal<Fluent>,
+             "predicate"_a,
+             "is_negated"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_role_atomic_goal_derived",
+             &dl::ConstructorRepositories::get_or_create_role_atomic_goal<Derived>,
+             "predicate"_a,
+             "is_negated"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_boolean_atomic_state_static",
+             &dl::ConstructorRepositories::get_or_create_boolean_atomic_state<Static>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_boolean_atomic_state_fluent",
+             &dl::ConstructorRepositories::get_or_create_boolean_atomic_state<Fluent>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_boolean_atomic_state_derived",
+             &dl::ConstructorRepositories::get_or_create_boolean_atomic_state<Derived>,
+             "predicate"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_boolean_nonempty_concept",
+             &dl::ConstructorRepositories::get_or_create_boolean_nonempty<dl::Concept>,
+             "constructor"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_boolean_nonempty_role",
+             &dl::ConstructorRepositories::get_or_create_boolean_nonempty<dl::Role>,
+             "constructor"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_numerical_count_concept",
+             &dl::ConstructorRepositories::get_or_create_numerical_count<dl::Concept>,
+             "constructor"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_numerical_count_role",
+             &dl::ConstructorRepositories::get_or_create_numerical_count<dl::Role>,
+             "constructor"_a,
+             nb::rv_policy::reference_internal)
+        .def("get_or_create_numerical_distance",
+             &dl::ConstructorRepositories::get_or_create_numerical_distance,
+             "left_concept"_a,
+             "role"_a,
+             "right_concept"_a,
+             nb::rv_policy::reference_internal);
+
+    nb::class_<dl::cnf_grammar::Grammar>(m, "Grammar")
+        .def(nb::init<const std::string&, formalism::Domain>(), "bnf_description"_a, "domain"_a)
+
+        // Static method
+        .def_static("create", &dl::cnf_grammar::Grammar::create, "type"_a, "domain"_a)
+
+        // Methods
+        .def("accept", &dl::cnf_grammar::Grammar::accept, "visitor"_a)
+
+        // Getters (returning references for efficiency)
+        .def("get_repositories", &dl::cnf_grammar::Grammar::get_repositories, nb::rv_policy::reference_internal)
+        .def("get_domain", &dl::cnf_grammar::Grammar::get_domain, nb::rv_policy::reference_internal);
+
+    /* ConstructorVisitor */
+    nb::class_<dl::Visitor, IPyDLVisitor>(m, "ConstructorVisitor")  //
+        .def(nb::init<>())
+        .def("visit", nb::overload_cast<dl::ConceptBot>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptTop>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptAtomicState<formalism::Static>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptAtomicState<formalism::Fluent>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptAtomicState<formalism::Derived>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptAtomicGoal<formalism::Static>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptAtomicGoal<formalism::Fluent>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptAtomicGoal<formalism::Derived>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptIntersection>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptUnion>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptNegation>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptValueRestriction>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptExistentialQuantification>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptRoleValueMapContainment>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptRoleValueMapEquality>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::ConceptNominal>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleUniversal>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleAtomicState<formalism::Static>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleAtomicState<formalism::Fluent>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleAtomicState<formalism::Derived>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleAtomicGoal<formalism::Static>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleAtomicGoal<formalism::Fluent>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleAtomicGoal<formalism::Derived>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleIntersection>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleUnion>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleComplement>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleInverse>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleComposition>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleTransitiveClosure>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleReflexiveTransitiveClosure>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleRestriction>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::RoleIdentity>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::BooleanAtomicState<formalism::Static>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::BooleanAtomicState<formalism::Fluent>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::BooleanAtomicState<formalism::Derived>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::BooleanNonempty<dl::Concept>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::BooleanNonempty<dl::Role>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::NumericalCount<dl::Concept>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::NumericalCount<dl::Role>>(&dl::Visitor::visit))
+        .def("visit", nb::overload_cast<dl::NumericalDistance>(&dl::Visitor::visit));
+}
 
 }
