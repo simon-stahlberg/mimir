a836c64c78ca1e0cdb19ef29a070f52ceaab5dbc
drexlerd <dominik.drexler@liu.se>
2025-03-16 08:02:40 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@e9ea1b28543bbf7996bc471804ad98d3ed52f385 🚀

--- search/classes_5.js
+++ search/classes_5.js
@@ -3,7 +3,7 @@ var searchData=
   ['fluent_0',['Fluent',['../structmimir_1_1formalism_1_1Fluent.html',1,'mimir::formalism']]],
   ['formattervisitor_1',['FormatterVisitor',['../classmimir_1_1languages_1_1dl_1_1cnf__grammar_1_1FormatterVisitor.html',1,'mimir::languages::dl::cnf_grammar::FormatterVisitor'],['../classmimir_1_1languages_1_1dl_1_1FormatterVisitor.html',1,'mimir::languages::dl::FormatterVisitor'],['../classmimir_1_1languages_1_1dl_1_1grammar_1_1FormatterVisitor.html',1,'mimir::languages::dl::grammar::FormatterVisitor']]],
   ['forward_2',['Forward',['../structmimir_1_1graphs_1_1Forward.html',1,'mimir::graphs']]],
-  ['fullactionformattertag_3',['FullActionFormatterTag',['../structmimir_1_1formalism_1_1FullActionFormatterTag.html',1,'mimir::formalism']]],
+  ['fullformattertag_3',['FullFormatterTag',['../structmimir_1_1formalism_1_1GroundActionImpl_1_1FullFormatterTag.html',1,'mimir::formalism::GroundActionImpl']]],
   ['functionexpressionbinaryoperatorimpl_4',['FunctionExpressionBinaryOperatorImpl',['../classmimir_1_1formalism_1_1FunctionExpressionBinaryOperatorImpl.html',1,'mimir::formalism']]],
   ['functionexpressionfunctionimpl_5',['FunctionExpressionFunctionImpl',['../classmimir_1_1formalism_1_1FunctionExpressionFunctionImpl.html',1,'mimir::formalism']]],
   ['functionexpressionimpl_6',['FunctionExpressionImpl',['../classmimir_1_1formalism_1_1FunctionExpressionImpl.html',1,'mimir::formalism']]],
--- search/classes_e.js
+++ search/classes_e.js
@@ -3,7 +3,7 @@ var searchData=
   ['parser_0',['Parser',['../classmimir_1_1formalism_1_1Parser.html',1,'mimir::formalism']]],
   ['pddlrepositories_1',['PDDLRepositories',['../classmimir_1_1formalism_1_1PDDLRepositories.html',1,'mimir::formalism']]],
   ['plan_2',['Plan',['../classmimir_1_1search_1_1Plan.html',1,'mimir::search']]],
-  ['planactionformattertag_3',['PlanActionFormatterTag',['../structmimir_1_1formalism_1_1PlanActionFormatterTag.html',1,'mimir::formalism']]],
+  ['planformattertag_3',['PlanFormatterTag',['../structmimir_1_1formalism_1_1GroundActionImpl_1_1PlanFormatterTag.html',1,'mimir::formalism::GroundActionImpl']]],
   ['predicateimpl_4',['PredicateImpl',['../classmimir_1_1formalism_1_1PredicateImpl.html',1,'mimir::formalism']]],
   ['problembuilder_5',['ProblemBuilder',['../classmimir_1_1formalism_1_1ProblemBuilder.html',1,'mimir::formalism']]],
   ['problemgoalcounter_6',['ProblemGoalCounter',['../classmimir_1_1search_1_1ProblemGoalCounter.html',1,'mimir::search']]],
