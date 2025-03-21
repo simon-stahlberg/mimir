ae56fd6855cdada75faf399e53fc3daf0885a704
drexlerd <dominik.drexler@liu.se>
2025-03-21 19:51:50 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@f320a5a0d00c84a0e76accdaacfa11983116c550 🚀

--- search/variables_d.js
+++ search/variables_d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['tuple_5fgraph_5foptions_0',['tuple_graph_options',['../structmimir_1_1datasets_1_1KnowledgeBaseImpl_1_1Options.html#a88e466f53e01f84109df6e1d6e73c048',1,'mimir::datasets::KnowledgeBaseImpl::Options']]]
+  ['timeout_5fms_0',['timeout_ms',['../structmimir_1_1datasets_1_1StateSpaceImpl_1_1Options.html#ad3c7c13274ea15f9886c25b0ee7471fa',1,'mimir::datasets::StateSpaceImpl::Options']]],
+  ['tuple_5fgraph_5foptions_1',['tuple_graph_options',['../structmimir_1_1datasets_1_1KnowledgeBaseImpl_1_1Options.html#a88e466f53e01f84109df6e1d6e73c048',1,'mimir::datasets::KnowledgeBaseImpl::Options']]]
 ];
