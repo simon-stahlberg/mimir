54f0229336a4131df74149d40e579b8c9aa3be8e
drexlerd <dominik.drexler@liu.se>
2025-03-21 08:54:13 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@d171f5a55de830aef85ee6a42c761b78b5dc4176 🚀

--- search/all_9.js
+++ search/all_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['key_5ftype_0',['key_type',['../structmimir_1_1graphs_1_1TrivialReadPropertyMap.html#a1a090c104357becaa17d3eb674897333',1,'mimir::graphs::TrivialReadPropertyMap::key_type()'],['../classmimir_1_1graphs_1_1VectorReadPropertyMap.html#aa710b47623d88751efcf6c75db70dc49',1,'mimir::graphs::VectorReadPropertyMap::key_type()'],['../classmimir_1_1graphs_1_1VectorReadWritePropertyMap.html#af00b649532ba669fbc6d5022eaa007fe',1,'mimir::graphs::VectorReadWritePropertyMap::key_type()'],['../classmimir_1_1graphs_1_1UnorderedMapReadPropertyMap.html#a519a8e6680ef82ad8d39492fa0eea122',1,'mimir::graphs::UnorderedMapReadPropertyMap::key_type()'],['../classmimir_1_1graphs_1_1UnorderedMapReadWritePropertyMap.html#a7bd2fdf0a5d465150e9424f8f8063400',1,'mimir::graphs::UnorderedMapReadWritePropertyMap::key_type()']]],
-  ['knowledge_5fbase_2ehpp_1',['knowledge_base.hpp',['../knowledge__base_8hpp.html',1,'']]],
-  ['knowledgebase_2',['KnowledgeBase',['../classmimir_1_1datasets_1_1KnowledgeBase.html#a5ac87a8ed4d19159b58051766b1c6b43',1,'mimir::datasets::KnowledgeBase::KnowledgeBase()'],['../classmimir_1_1datasets_1_1KnowledgeBase.html',1,'mimir::datasets::KnowledgeBase']]]
+  ['keyvaluemap_1',['KeyValueMap',['../namespacemimir.html#abff7428eb26239bc927eb869830c7dda',1,'mimir']]],
+  ['knowledge_5fbase_2ehpp_2',['knowledge_base.hpp',['../knowledge__base_8hpp.html',1,'']]],
+  ['knowledgebase_3',['KnowledgeBase',['../classmimir_1_1datasets_1_1KnowledgeBase.html#a5ac87a8ed4d19159b58051766b1c6b43',1,'mimir::datasets::KnowledgeBase::KnowledgeBase()'],['../classmimir_1_1datasets_1_1KnowledgeBase.html',1,'mimir::datasets::KnowledgeBase']]]
 ];
