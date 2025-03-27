fb515bfa7d3cfdcdb5254b518736e98980df84de
drexlerd <dominik.drexler@liu.se>
2025-03-27 18:02:11 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@3fbed03b5583277d11b390cc86e13c74da3f893e 🚀

--- search/variables_6.js
+++ search/variables_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['initial_5ftable_5fatoms_0',['INITIAL_TABLE_ATOMS',['../namespacemimir_1_1search_1_1iw.html#a69bcf737af67e2786bc4dc46a5edb2d0',1,'mimir::search::iw']]],
-  ['is_5fnegated_1',['is_negated',['../structmimir_1_1formalism_1_1GroundEffectDerivedLiteral.html#a32ee51b3a0a0f7e592451e816d970bf8',1,'mimir::formalism::GroundEffectDerivedLiteral']]]
+  ['initial_5ftable_5fatoms_0',['INITIAL_TABLE_ATOMS',['../namespacemimir_1_1search_1_1iw.html#a69bcf737af67e2786bc4dc46a5edb2d0',1,'mimir::search::iw']]]
 ];
