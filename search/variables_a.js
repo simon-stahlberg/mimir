24371e6ca3913ce3c5a6f05e7d016bebadfc54f1
drexlerd <dominik.drexler@liu.se>
2025-03-29 20:24:21 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@c5e7e5eec27603a45f6280e32f01b4bf7e7cf596 🚀

--- search/variables_a.js
+++ search/variables_a.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['plan_0',['plan',['../structmimir_1_1search_1_1SearchResult.html#ad81d2706f5870a1fd4e27b5651953f4c',1,'mimir::search::SearchResult']]],
   ['polarity_1',['polarity',['../structmimir_1_1formalism_1_1GroundEffectDerivedLiteral.html#a11a9c104aaecb4b1a75a99e868d6a73c',1,'mimir::formalism::GroundEffectDerivedLiteral']]],
-  ['predecessors_2',['predecessors',['../structmimir_1_1graphs_1_1bgl_1_1BFSBoostVisitor.html#a6d0362cc05df69f4328b29dc1836ddc7',1,'mimir::graphs::bgl::BFSBoostVisitor::predecessors()'],['../structmimir_1_1graphs_1_1bgl_1_1DFSBoostVisitor.html#a6ae4b8fcfa9848346d3c9f6f757780d5',1,'mimir::graphs::bgl::DFSBoostVisitor::predecessors()']]]
+  ['predecessors_2',['predecessors',['../structmimir_1_1graphs_1_1bgl_1_1BFSBoostVisitor.html#a6d0362cc05df69f4328b29dc1836ddc7',1,'mimir::graphs::bgl::BFSBoostVisitor::predecessors()'],['../structmimir_1_1graphs_1_1bgl_1_1DefaultDFSBoostVisitor.html#ab772733bdff141698becb972c4192268',1,'mimir::graphs::bgl::DefaultDFSBoostVisitor::predecessors()']]]
 ];
