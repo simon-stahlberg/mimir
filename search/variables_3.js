8cb1909a5fbef8c953260306eb8bf8098fc2cea7
drexlerd <dominik.drexler@liu.se>
2025-03-29 20:49:08 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@72157f77ebd6ad976ba4784ac1de968f284f144a 🚀

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['distances_0',['distances',['../structmimir_1_1graphs_1_1bgl_1_1BFSBoostVisitor.html#ac9f99532d7a6ebae20026b606aaa7a99',1,'mimir::graphs::bgl::BFSBoostVisitor::distances()'],['../structmimir_1_1graphs_1_1bgl_1_1DefaultDFSBoostVisitor.html#ab6303d966fc53d33999de3f574d627d1',1,'mimir::graphs::bgl::DefaultDFSBoostVisitor::distances()']]]
+  ['distances_0',['distances',['../structmimir_1_1graphs_1_1bgl_1_1BFSBoostVisitor.html#ac9f99532d7a6ebae20026b606aaa7a99',1,'mimir::graphs::bgl::BFSBoostVisitor']]]
 ];
