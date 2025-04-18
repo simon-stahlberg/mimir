f0d25bf401224ee47aa5b9c5bad0a4af7f3e0a22
drexlerd <dominik.drexler@liu.se>
2025-04-18 23:07:11 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@5086e10e95b054106079febaa920b5d6dab8619b 🚀

--- search/files_b.js
+++ search/files_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['metric_2ehpp_0',['metric.hpp',['../formalism_2metric_8hpp.html',1,'(Global Namespace)'],['../search_2metric_8hpp.html',1,'(Global Namespace)']]]
+  ['metric_2ehpp_0',['metric.hpp',['../metric_8hpp.html',1,'']]]
 ];
