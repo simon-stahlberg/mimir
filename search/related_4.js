c848190b812d7f96bbba9326642c5876a2a7cdad
drexlerd <dominik.drexler@liu.se>
2025-04-19 17:55:37 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@fa588bb4b1fce36bde1aa6eceaa73f10014e3302 🚀

--- search/related_4.js
+++ search/related_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c_0',['operator&lt;&lt;',['../classmimir_1_1graphs_1_1nauty_1_1SparseGraph.html#a3a74dbd72599f661693c7c58844bd464',1,'mimir::graphs::nauty::SparseGraph::operator&lt;&lt;()'],['../classmimir_1_1graphs_1_1Color.html#aeec2f77668ab256b31a23b2514307b10',1,'mimir::graphs::Color::operator&lt;&lt;()']]]
+  ['operator_3c_3c_0',['operator&lt;&lt;',['../classmimir_1_1graphs_1_1nauty_1_1SparseGraph.html#a3a74dbd72599f661693c7c58844bd464',1,'mimir::graphs::nauty::SparseGraph']]]
 ];
