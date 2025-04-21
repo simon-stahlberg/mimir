090ddb91b61aa00d1f8d4997bbc319a6076319c1
drexlerd <dominik.drexler@liu.se>
2025-04-21 18:07:28 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@a1b60f7bc9ec2d8e744ecc2f58bd0346281bf81a 🚀

--- search/variables_b.js
+++ search/variables_b.js
@@ -6,5 +6,5 @@ var searchData=
   ['state_5fto_5fcert_3',['state_to_cert',['../structmimir_1_1datasets_1_1CertificateMaps.html#a7c8f5ac251fc045ac0842f6e715c9046',1,'mimir::datasets::CertificateMaps']]],
   ['statistics_4',['statistics',['../structmimir_1_1search_1_1applicable__action__generator_1_1grounded_1_1Statistics.html#a021e9e7c580f2e40b7a43a4412368f55',1,'mimir::search::applicable_action_generator::grounded::Statistics']]],
   ['status_5',['status',['../structmimir_1_1search_1_1SearchResult.html#af34a3ff103ec0bf69d6aca72efa1a7d3',1,'mimir::search::SearchResult']]],
-  ['symmetry_5fpruning_6',['symmetry_pruning',['../structmimir_1_1datasets_1_1generalized__state__space_1_1Options.html#a503241358548c33a567c45a09fcb7f1c',1,'mimir::datasets::generalized_state_space::Options::symmetry_pruning()'],['../structmimir_1_1datasets_1_1state__space_1_1Options.html#a4ee4f2dcb3265d9797368d9138883c97',1,'mimir::datasets::state_space::Options::symmetry_pruning()']]]
+  ['symmetry_5fpruning_6',['symmetry_pruning',['../structmimir_1_1datasets_1_1state__space_1_1Options.html#a4ee4f2dcb3265d9797368d9138883c97',1,'mimir::datasets::state_space::Options']]]
 ];
