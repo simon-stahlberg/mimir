fa129deaedde3c99c997faec83ac93c73a8ba686
drexlerd <dominik.drexler@liu.se>
2025-05-01 13:02:50 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@b2a548d721a9e066f41d290606f4b274a593fb76 🚀

--- search/enumvalues_0.js
+++ search/enumvalues_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['closed_0',['CLOSED',['../namespacemimir_1_1search.html#a2097fb91f235e64558eedaf6b1aa165aa9eaa9bcce4787b02906847bda11f4f70',1,'mimir::search']]],
-  ['cyclic_1',['CYCLIC',['../namespacemimir_1_1languages_1_1general__policies.html#a95ee7da82de8f0176ca509a0d11819e2ab970fa647bc2f4761a80276428780061',1,'mimir::languages::general_policies']]]
+  ['complete_1',['COMPLETE',['../namespacemimir_1_1languages_1_1dl_1_1cnf__grammar.html#a54bc6efb7b7df0b59e8289156f4e7f6da3de44296982e58199afc513a715b12ba',1,'mimir::languages::dl::cnf_grammar']]],
+  ['cyclic_2',['CYCLIC',['../namespacemimir_1_1languages_1_1general__policies.html#a95ee7da82de8f0176ca509a0d11819e2ab970fa647bc2f4761a80276428780061',1,'mimir::languages::general_policies']]]
 ];
