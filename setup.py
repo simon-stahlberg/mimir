c69a7f7ef1db95142e21772280a8184b501459fe
Dominik Drexler <drexlerd93@gmail.com>
2025-05-22 18:24:35 +0200

    python vesion

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
 
-__version__ = "0.13.19"
+__version__ = "0.13.20"
 HERE = Path(__file__).resolve().parent
 
 
