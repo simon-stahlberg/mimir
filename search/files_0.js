91a6ea75620c9ba59e91858738c59c740a37c840
drexlerd <dominik.drexler@liu.se>
2025-05-31 15:59:23 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@8af106bb6e245fb299f9de8ec29e6c9e29e99fca 🚀

--- search/files_0.js
+++ search/files_0.js
@@ -4,8 +4,7 @@ var searchData=
   ['add_2ehpp_1',['add.hpp',['../add_8hpp.html',1,'']]],
   ['applicability_2ehpp_2',['applicability.hpp',['../applicability_8hpp.html',1,'']]],
   ['assignment_5fset_2ehpp_3',['assignment_set.hpp',['../assignment__set_8hpp.html',1,'']]],
-  ['astar_2ehpp_4',['astar.hpp',['../astar_8hpp.html',1,'']]],
-  ['atom_2ehpp_5',['atom.hpp',['../atom_8hpp.html',1,'']]],
-  ['axiom_2ehpp_6',['axiom.hpp',['../formalism_2axiom_8hpp.html',1,'(Global Namespace)'],['../search_2satisficing__binding__generators_2axiom_8hpp.html',1,'(Global Namespace)']]],
-  ['axiom_5fstratification_2ehpp_7',['axiom_stratification.hpp',['../axiom__stratification_8hpp.html',1,'']]]
+  ['atom_2ehpp_4',['atom.hpp',['../atom_8hpp.html',1,'']]],
+  ['axiom_2ehpp_5',['axiom.hpp',['../formalism_2axiom_8hpp.html',1,'(Global Namespace)'],['../search_2satisficing__binding__generators_2axiom_8hpp.html',1,'(Global Namespace)']]],
+  ['axiom_5fstratification_2ehpp_6',['axiom_stratification.hpp',['../axiom__stratification_8hpp.html',1,'']]]
 ];
