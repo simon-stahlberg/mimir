38e26cf64491b2218ae4e83c1bf4b2af0bc480b9
Dominik Drexler <drexlerd93@gmail.com>
2025-06-03 14:20:12 +0200

    update python version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
 
-__version__ = "0.13.26"
+__version__ = "0.13.27"
 HERE = Path(__file__).resolve().parent
 
 
