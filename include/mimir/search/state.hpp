490b2e81c6db61690d468cbe36ca53c5f2e8b995
Dominik Drexler <drexlerd93@gmail.com>
2025-06-13 09:05:05 +0200

    try delta encoding

--- include/mimir/search/state.hpp
+++ include/mimir/search/state.hpp
@@ -38,7 +38,7 @@
 
 namespace mimir::search
 {
-namespace v = valla::canonical;
+namespace v = valla::canonical_delta;
 
 /// @brief `StateImpl` encapsulates the fluent and derived atoms, and numeric variables of a planning state.
 class StateImpl
