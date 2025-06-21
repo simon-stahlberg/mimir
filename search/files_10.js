427976a0ace41b3a4c6fd1dd83a106d54c41ab65
drexlerd <dominik.drexler@liu.se>
2025-06-21 16:29:31 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@356f11465199996b69d9803c0c7e181e90213ce7 🚀

--- search/files_10.js
+++ search/files_10.js
@@ -2,9 +2,8 @@ var searchData=
 [
   ['tags_2ehpp_0',['tags.hpp',['../formalism_2tags_8hpp.html',1,'(Global Namespace)'],['../languages_2description__logics_2tags_8hpp.html',1,'(Global Namespace)']]],
   ['term_2ehpp_1',['term.hpp',['../term_8hpp.html',1,'']]],
-  ['translator_2ehpp_2',['translator.hpp',['../translator_8hpp.html',1,'']]],
-  ['tuple_5fgraph_2ehpp_3',['tuple_graph.hpp',['../tuple__graph_8hpp.html',1,'']]],
-  ['tuple_5findex_5fgenerators_2ehpp_4',['tuple_index_generators.hpp',['../tuple__index__generators_8hpp.html',1,'']]],
-  ['tuple_5findex_5fmapper_2ehpp_5',['tuple_index_mapper.hpp',['../tuple__index__mapper_8hpp.html',1,'']]],
-  ['types_2ehpp_6',['types.hpp',['../common_2types_8hpp.html',1,'(Global Namespace)'],['../search_2algorithms_2iw_2types_8hpp.html',1,'(Global Namespace)']]]
+  ['tuple_5fgraph_2ehpp_2',['tuple_graph.hpp',['../tuple__graph_8hpp.html',1,'']]],
+  ['tuple_5findex_5fgenerators_2ehpp_3',['tuple_index_generators.hpp',['../tuple__index__generators_8hpp.html',1,'']]],
+  ['tuple_5findex_5fmapper_2ehpp_4',['tuple_index_mapper.hpp',['../tuple__index__mapper_8hpp.html',1,'']]],
+  ['types_2ehpp_5',['types.hpp',['../common_2types_8hpp.html',1,'(Global Namespace)'],['../search_2algorithms_2iw_2types_8hpp.html',1,'(Global Namespace)']]]
 ];
