fb5a1a193371bf6d66569bf02644e66145d5e793
Dominik Drexler <drexlerd93@gmail.com>
2025-07-14 15:32:00 +0200

    update output

--- exe/planner_astar.cpp
+++ exe/planner_astar.cpp
@@ -185,24 +185,37 @@ int main(int argc, char** argv)
     std::cout << "Peak memory usage in bytes: " << get_peak_memory_usage_in_bytes() << std::endl;
     std::cout << "Number of index slots: " << problem->get_index_tree_table().size() << std::endl;
     std::cout << "Number of double slots: " << problem->get_double_tree_table().size() << std::endl;
-    uint64_t num_fluent_state_atoms = 0;
-    uint64_t num_derived_state_atoms = 0;
+    std::cout << "Number of slots: " << problem->get_index_tree_table().size() + problem->get_double_tree_table().size() << std::endl;
+    uint64_t num_fluent_state_variables = 0;
+    uint64_t num_derived_state_variables = 0;
+    uint64_t num_numeric_state_variables = 0;
     for (const auto& [packed_state, index] : state_repository->get_states())
     {
         auto state = state_repository->get_state(packed_state);
-        num_fluent_state_atoms += state.get_atoms<formalism::FluentTag>().count();
-        num_derived_state_atoms += state.get_atoms<formalism::DerivedTag>().count();
+        num_fluent_state_variables += state.get_atoms<formalism::FluentTag>().count();
+        num_derived_state_variables += state.get_atoms<formalism::DerivedTag>().count();
+        num_numeric_state_variables += state.get_numeric_variables().size();
     }
 
-    std::cout << "Average number of fluent state atoms: " << static_cast<double>(num_fluent_state_atoms) / state_repository->get_state_count() << std::endl;
-    std::cout << "Average number of derived state atoms: " << static_cast<double>(num_derived_state_atoms) / state_repository->get_state_count() << std::endl;
-    std::cout << "Average number of state atoms: "
-              << static_cast<double>(num_fluent_state_atoms + num_derived_state_atoms) / state_repository->get_state_count() << std::endl;
+    std::cout << "Average number of fluent state variables: " << static_cast<double>(num_fluent_state_variables) / state_repository->get_state_count()
+              << std::endl;
+    std::cout << "Average number of derived state variables: " << static_cast<double>(num_derived_state_variables) / state_repository->get_state_count()
+              << std::endl;
+    std::cout << "Average number of numeric state variables: " << static_cast<double>(num_numeric_state_variables) / state_repository->get_state_count()
+              << std::endl;
+    std::cout << "Average number of state variables: "
+              << static_cast<double>(num_fluent_state_variables + num_derived_state_variables + num_numeric_state_variables)
+                     / state_repository->get_state_count()
+              << std::endl;
 
     std::cout << "Average number of index slots per state: "
               << static_cast<double>(problem->get_index_tree_table().size()) / state_repository->get_state_count() << std::endl;
     std::cout << "Average number of double slots per state: "
               << static_cast<double>(problem->get_double_tree_table().size()) / state_repository->get_state_count() << std::endl;
+    std::cout << "Average number of slots per state: "
+              << (static_cast<double>(problem->get_index_tree_table().size()) + static_cast<double>(problem->get_double_tree_table().size()))
+                     / state_repository->get_state_count()
+              << std::endl;
 
     if (result.status == SearchStatus::SOLVED)
     {
