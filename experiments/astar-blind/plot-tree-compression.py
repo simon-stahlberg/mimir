ef6356afa267f7d478ba374eaf29e7f09ee04c57
Dominik Drexler <drexlerd93@gmail.com>
2025-07-29 19:53:12 +0200

    results

--- experiments/astar-blind/plot-tree-compression.py
+++ experiments/astar-blind/plot-tree-compression.py
@@ -4,8 +4,8 @@ import json
 from collections import defaultdict
 
 def main():
-    # with open("combined-astar-blind-30-propositional-eval/properties", 'r') as file:
-    with open("combined-astar-blind-30-numeric-eval/properties", 'r') as file:
+    with open("combined-astar-blind-30-propositional-eval/properties", 'r') as file:
+    # with open("combined-astar-blind-30-numeric-eval/properties", 'r') as file:
         data = json.load(file)
 
         task_to_runs = defaultdict(list)
@@ -27,12 +27,18 @@ def main():
             list_avg_num_state_variables = None
             for run in runs:
                 if run["algorithm"] == "list-lifted-astar-eager-blind":
-                    list_peak_mem = run["state_peak_memory_usage_in_bytes"]
-                    list_avg_num_state_variables = run["average_num_state_variables"] 
+                    if "state_peak_memory_usage_in_bytes" in run:
+                        list_peak_mem = run["state_peak_memory_usage_in_bytes"]
+                    if "average_num_state_variables" in run:
+                        list_avg_num_state_variables = run["average_num_state_variables"] 
                 elif run["algorithm"] == "tree-swiss-lifted-astar-eager-blind":
-                    tree_peak_mem = run["state_peak_memory_usage_in_bytes"]
+                    print(run)
+                    if "state_peak_memory_usage_in_bytes" in run:
+                        tree_peak_mem = run["state_peak_memory_usage_in_bytes"]
 
-            assert(list_peak_mem is not None and tree_peak_mem is not None and list_avg_num_state_variables is not None)
+            if list_peak_mem is None or tree_peak_mem is None or list_avg_num_state_variables is None:
+                continue
+            
             X.append(list_avg_num_state_variables)
             Y.append(tree_peak_mem / list_peak_mem)
 
