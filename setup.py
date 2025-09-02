0e57d4c88bfe6bb6cc17214f130f3e84c0e1dceb
Dominik Drexler <drexlerd93@gmail.com>
2025-09-02 13:21:38 +0200

    python version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
 
-__version__ = "0.13.50"
+__version__ = "0.13.51"
 HERE = Path(__file__).resolve().parent
 
 
