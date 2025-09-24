d3f548963ab0fc2a16963bf4501ebeecbba17179
Dominik Drexler <drexlerd93@gmail.com>
2025-09-24 17:17:45 +0200

    added compile flags and current data

--- src/formalism/problem.cpp
+++ src/formalism/problem.cpp
@@ -251,11 +251,11 @@ size_t ProblemImpl::get_estimated_memory_usage_in_bytes() const
  * Additional members
  */
 
-valla::IndexedHashSet<valla::Slot<Index>, Index>& ProblemImpl::get_index_tree_table() { return m_index_tree_table; }
-const valla::IndexedHashSet<valla::Slot<Index>, Index>& ProblemImpl::get_index_tree_table() const { return m_index_tree_table; }
+IndexTreeTable& ProblemImpl::get_index_tree_table() { return m_index_tree_table; }
+const IndexTreeTable& ProblemImpl::get_index_tree_table() const { return m_index_tree_table; }
 
-valla::IndexedHashSet<double, Index>& ProblemImpl::get_double_leaf_table() { return m_double_leaf_table; }
-const valla::IndexedHashSet<double, Index>& ProblemImpl::get_double_leaf_table() const { return m_double_leaf_table; }
+DoubleLeafTable& ProblemImpl::get_double_leaf_table() { return m_double_leaf_table; }
+const DoubleLeafTable& ProblemImpl::get_double_leaf_table() const { return m_double_leaf_table; }
 
 std::pair<const FlatIndexList*, Index> ProblemImpl::get_or_create_index_list(const FlatIndexList& list)
 {
