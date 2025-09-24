f80e98769610b5a581a1d15a160b7b5f8f8ad592
Dominik Drexler <drexlerd93@gmail.com>
2025-09-24 15:26:08 +0200

    update compile flag name and scripts

--- experiments/astar-blind/generators/2025-09-19-lifted-exhaustive-astar30-ipc-numeric.py
+++ experiments/astar-blind/generators/2025-09-19-lifted-exhaustive-astar30-ipc-numeric.py
@@ -192,7 +192,7 @@ for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
     # 'domain', 'problem', 'algorithm', 'coverage'.
     run.set_property("domain", task.domain)
     run.set_property("problem", task.problem)
-    run.set_property("algorithm", f"mimir-{lifted_kind}-astar-eager-blind")
+    run.set_property("algorithm", f"mimir-lifted-{lifted_kind}-astar-eager-blind")
     # BaseReport needs the following properties:
     # 'time_limit', 'memory_limit'.
     run.set_property("time_limit", TIME_LIMIT)
@@ -200,7 +200,7 @@ for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
     # Every run has to have a unique id in the form of a list.
     # The algorithm name is only really needed when there are
     # multiple algorithms.
-    run.set_property("id", [f"mimir-{lifted_kind}-astar-eager-blind", task.domain, task.problem])
+    run.set_property("id", [f"mimir-lifted-{lifted_kind}-astar-eager-blind", task.domain, task.problem])
 
 # Add step that writes experiment files to disk.
 exp.add_step("build", exp.build)
