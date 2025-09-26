ec74acea57ded1b165c423ce60d4cf47fd544245
Dominik Drexler <drexlerd93@gmail.com>
2025-09-26 09:54:00 +0200

    fix shiping of src distribution

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
 
-__version__ = "0.13.52"
+__version__ = "0.13.53"
 HERE = Path(__file__).resolve().parent
 
 
