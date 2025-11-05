d05eb5a140771bc49ef00638e457f507b9a32243
drexlerd <dominik.drexler@liu.se>
2025-11-05 15:55:01 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@379073f71141caf2f95bbdf6ac71c40f3c1b9a3f 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['maxheuristicimpl_0',['MaxHeuristicImpl',['../classmimir_1_1search_1_1MaxHeuristicImpl.html#a757ee176fe79cd837eb04defe8e803af',1,'mimir::search::MaxHeuristicImpl']]]
+  ['maxheuristicimpl_0',['MaxHeuristicImpl',['../classmimir_1_1search_1_1MaxHeuristicImpl.html#a44d4065695bc3dc172c1dfa849b65155',1,'mimir::search::MaxHeuristicImpl']]]
 ];
