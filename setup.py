2e911640427c17ef521a7564877abc7df80cea93
Simon <simon.stahlberg@gmail.com>
2025-11-24 09:54:14 +0100

    Update Python version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
 
 
-__version__ = "0.13.55"
+__version__ = "0.13.56"
 HERE = Path(__file__).resolve().parent
 
 
