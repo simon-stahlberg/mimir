805f8f018af832b40715addce9dc0213f6a32c5e
drexlerd <dominik.drexler@liu.se>
2026-03-09 17:15:53 +0000

    Deploying to gh-pages from @ simon-stahlberg/mimir@f052fa32c17ae745606db0c9d02b526633c9caba 🚀

--- search/variables_9.js
+++ search/variables_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['object_0',['object',['../structmimir_1_1formalism_1_1VertexAssignment.html#a602bd09114b70150e8c289f70aa8f7e2',1,'mimir::formalism::VertexAssignment']]],
-  ['openlist_5fweights_1',['openlist_weights',['../structmimir_1_1search_1_1astar__lazy_1_1Options.html#a317edf6995c17ff22cb8fd743526456e',1,'mimir::search::astar_lazy::Options::openlist_weights()'],['../structmimir_1_1search_1_1gbfs__lazy_1_1Options.html#a546674f3250d8d2d19ef8a2334aa2cd4',1,'mimir::search::gbfs_lazy::Options::openlist_weights()']]],
+  ['openlist_5fweights_1',['openlist_weights',['../structmimir_1_1search_1_1astar__lazy_1_1Options.html#a317edf6995c17ff22cb8fd743526456e',1,'mimir::search::astar_lazy::Options::openlist_weights()'],['../structmimir_1_1search_1_1gbfs__lazy_1_1Options.html#a546674f3250d8d2d19ef8a2334aa2cd4',1,'mimir::search::gbfs_lazy::Options::openlist_weights()'],['../structmimir_1_1search_1_1gbfs__eager_1_1Options.html#ad329945882117b50c811152f0fe9bcfc',1,'mimir::search::gbfs_eager::Options::openlist_weights()']]],
   ['optimization_5fdirection_2',['optimization_direction',['../structmimir_1_1search_1_1match__tree_1_1Options.html#ace70aedf0d97f4e3a63c61dd61d8f5d8',1,'mimir::search::match_tree::Options']]],
   ['output_5fdot_5ffile_3',['output_dot_file',['../structmimir_1_1search_1_1match__tree_1_1Options.html#a31509aa1b133ec8de7cb842fb77f849e',1,'mimir::search::match_tree::Options']]]
 ];
